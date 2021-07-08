f6cf854d5e3575a77c37c713adb5dea6371d3c49
Jake Seunghun Kim <sky9173546@gmail.com>
2021-07-07 18:16:56 -0700

    skeleton about and home

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>About me</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
